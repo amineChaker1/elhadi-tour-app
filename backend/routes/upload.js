5edc1759f4ee77ea6ccf0a6a11a95b88af5d30bc
Chaker Amine <chkramine2003@gmail.com>
2023-04-15 02:05:19 +0100

    moved project

--- backend/routes/upload.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { imageUploader } from "../controllers/upload.js";
-
-const router = express.Router();
-
-router.post("/image", imageUploader);
-export default router;
