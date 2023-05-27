b24d11706544b333b73f6cc93284b36006688c41
Chaker Amine <chkramine2003@gmail.com>
2023-05-27 15:54:54 +0100

    fixed booking section

--- frontend/src/components/BookingSection.jsx
+++ frontend/src/components/BookingSection.jsx
@@ -33,7 +33,7 @@ const BookingSection = () => {
             <div className="w-48">
               <PlaceImg place={booking.place} />
             </div>
-            <div className="py-3 pr-3 grow">
+            <div className="py-3 pr-3 w-48 grow">
               <h2 className="text-xl">{booking.place.title}</h2>
               <div className="text-xl">
                 <BookingDates
@@ -55,7 +55,7 @@ const BookingSection = () => {
                       d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
                     />
                   </svg>
-                  <span className="text-2xl">{booking.price} DA</span>
+                  <span className="text-2xl">{booking.price} دينار</span>
                 </div>
               </div>
             </div>
